db05c3cf36894e00099b0d76521046ffaf2bd4e8
Nathan Szurek <nathan.szurek@yahoo.com>
2020-07-03 18:58:57 -0500

    Expanded bootstrap carousel

---
+++ assets/home.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+    // Activate Carousel
+    $("#JS30").carousel({ interval: 7000 });
+
+    // Enable Carousel Indicators
+    $(".item1").click(function () {
+        $("#myCarousel").carousel(0);
+    });
+
+    $(".item2").click(function () {
+        $("#myCarousel").carousel(1);
+    });
+
+    $(".item3").click(function () {
+        $("#myCarousel").carousel(2);
+    });
+
+    $(".item4").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    $(".item5").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    $(".item6").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    $(".item7").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    $(".item8").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    $(".item9").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    $(".item10").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+})
